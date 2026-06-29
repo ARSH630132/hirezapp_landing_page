d4788bb43a2f9e15073d1b8e7b0b5140e16c64e2
ARSH630132 <200009560+ARSH630132@users.noreply.github.com>
2026-06-29 11:35:28 +0530

    fix:footer social media logo color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -114,7 +114,7 @@ export default function Footer() {
                       aria-label={social.name}
                       className="flex h-10 w-10 items-center justify-center rounded-full border border-white/10 bg-white/5 hover:border-[#009DFF]/40 hover:bg-white/10"
                     >
-                      <img src={social.icon} alt={social.name} className="h-4 w-4 object-contain" />
+                      <img src={social.icon} alt={social.name}       className="h-4 w-4 object-contain brightness-0 invert" />
                     </a>
                   ))}
                 </div>
